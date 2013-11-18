181a3f6a3cc5a9e28846f8f479b9196c0a6b8338
Michael Andersen <michael@steelcode.com>
2013-11-17 16:07:08 -0800

    Add pseudo-data generation functions

--- smapexplorer/views.py
+++ smapexplorer/views.py
@@ -1,4 +1,5 @@
 from pyramid.view import view_config
+from smapx_data import *
 
 @view_config(route_name='index', renderer='index.mako')
 def v_index(request):
@@ -7,6 +8,16 @@ def v_index(request):
     
 @view_config(route_name='data', renderer='json')
 def v_data(request):
-    print request.GET
-    print request.POST
-    return {'data':range(50)}
+    for e in ["starttime", "endtime", "unit", "resampleto"]:
+        if e not in request.GET:
+            request.response.status = 400
+            return {'status':'error','message':'expected field %s'%e}
+    try:
+        rv = get_smapx_data(request.GET["starttime"],
+                            request.GET["endtime"],
+                            request.GET["unit"],
+                            request.GET["resampleto"])
+        return rv
+    except Exception as e:
+        request.response.status = 400
+        return {'status':'error', 'message':str(e)}
