03599c1c25b200120cf44a77761773f0e4fc7320
Nicholas Sun <olassun2@gmail.com>
2013-11-23 20:08:20 -0800

    dummy graph rendered

--- smapexplorer/static/js/smapx.js
+++ smapexplorer/static/js/smapx.js
@@ -1,20 +1,96 @@
+// count, max, mean, min, mt, tw
+var starttime = 500,
+    endtime = 3000,
+    unit = "ms",
+    resampleto = 1000;
 
-var data = [1,2,3,5,3,2];
-
-d3.select("#chart")
-  .selectAll("div")
-    .data(data)
-  .enter().append("div")
-    .style("width", function(d) { return d * 100 + "px"; } )
-    .text(function(d) { return d; } );
-    
 $().ready(function()
 {
-    $.getJSON("/data",{'start':500,'end':3000,'subsample':1000},
-        function(data, status, xhr)
-        {
-            console.log("retval: ",data);
-            $('#target').text(data["data"]);
-        });
-  
+    $.getJSON("/data",{'starttime':starttime,'endtime':endtime,'unit':unit,'resampleto':resampleto},
+    function(sample, status, xhr)
+    {
+        var times = new Array(sample.length),
+            means = new Array(sample.length),
+            mins = new Array(sample.length),
+            maxes = new Array(sample.length);
+        for(var i = 0; i < sample.length; i++) {
+            times[i] = sample[i].mt;
+            means[i] = sample[i].mean;
+            mins[i] = sample[i].min;
+            maxes[i] = sample[i].max;
+        }
+
+        var margin = {top: 20, right: 20, bottom: 30, left: 50},
+            width = 960 - margin.left - margin.right,
+            height = 500 - margin.top - margin.bottom;
+
+        var svgContainer = d3.select("#chart").append("svg")
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+        
+        /*
+        var xaxis_sel = svgContainer.select("xaxis")
+            .append("svg")
+            .attr("class", "xaxis")
+            .attr("width", 1440)
+            .attr("height",30)
+            .append("g")
+            .attr("transform", "translate(0,30)")
+            .call(axis);
+            */
+
+    /*
+        var yaxis_sel = svgContainer.select("yaxis")
+            .data(means)
+            .enter()
+            .append("ypoint");
+            */
+
+        var x = d3.scale.linear()
+            .domain([starttime, endtime])
+            .range([0, width]);
+
+        var max = Math.max.apply(Math, means),
+            min = Math.min.apply(Math, means);
+
+        var y = d3.scale.linear()
+            .domain([min,max])
+            .range([height,0]);
+
+        var dx = d3.scale.linear()
+            .range([0, width-100]);
+
+        var xaxis = d3.svg.axis()
+            .scale(x)
+            .orient("bottom");
+
+        var yaxis = d3.svg.axis()
+            .scale(y)
+            .orient("left");
+
+        var line = d3.svg.line()
+            //.x(function(d) { return (d.mt).toFixed(2); })
+            //.y(function(d) { return (d.mean).toFixed(2); })
+            .x(function(d) { return x(d.mt); })
+            .y(function(d) { return y(d.mean); })
+            .interpolate("linear");
+
+        var xaxis_graph = svgContainer.append("g")
+            .attr("class", "x_axis")
+            .attr("transform", "translate(" + 0 + "," + (height) + ")")
+            .call(xaxis);
+
+        var yaxis_graph = svgContainer.append("g")
+            .attr("class", "y_axis")
+            .attr("transform", "translate(" + 0 + "," + 0 + ")")
+            .call(yaxis);
+
+        var linegraph = svgContainer.append("path")
+            .attr("class", "line")
+            //.attr("clip-path", "url(#clip)")
+            .attr("transform", "translate(" + 0 + "," + 0 + ")")
+            .attr("d", line(sample));
+    });
 });
--- smapexplorer/templates/index.mako
+++ smapexplorer/templates/index.mako
@@ -75,12 +75,12 @@
       <!-- Main component for a primary marketing message or call to action -->
       <div class="jumbotron">
         <h1>Navbar example</h1>
-        <p>
+        <!-- <p>
          %for i in range(50):
             <% p = "'<><$%@#$#$<'" %>
               <p>${"Hello %s" %p}</p>
          %endfor
-         </p>
+         </p>-->
            <p id="target">Unloaded</p>
         <p>This example is a quick exercise to illustrate how the default, static and fixed to top navbar work. It includes the responsive CSS and HTML, so it also adapts to your viewport and device.</p>
         <p>To see the difference between static and fixed top navbars, just scroll.</p>
