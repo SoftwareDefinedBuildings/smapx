6b2ec71990fd31b29db53a52dc532071ecbb0ba3
Nicholas Sun <olassun2@gmail.com>
2013-11-24 01:00:19 -0800

    line moves and zooms, x axis doesn't follow though

--- smapexplorer/static/js/smapx.js
+++ smapexplorer/static/js/smapx.js
@@ -4,6 +4,8 @@ var starttime = 500,
     unit = "ms",
     resampleto = 1000;
 
+
+
 $().ready(function()
 {
     $.getJSON("/data",{'starttime':starttime,'endtime':endtime,'unit':unit,'resampleto':resampleto},
@@ -14,7 +16,7 @@ $().ready(function()
             mins = new Array(sample.length),
             maxes = new Array(sample.length);
         for(var i = 0; i < sample.length; i++) {
-            times[i] = sample[i].mt;
+            //times[i] = sample[i].mt;
             means[i] = sample[i].mean;
             mins[i] = sample[i].min;
             maxes[i] = sample[i].max;
@@ -29,68 +31,72 @@ $().ready(function()
             .attr("height", height + margin.top + margin.bottom)
         .append("g")
             .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-        
-        /*
-        var xaxis_sel = svgContainer.select("xaxis")
-            .append("svg")
-            .attr("class", "xaxis")
-            .attr("width", 1440)
-            .attr("height",30)
-            .append("g")
-            .attr("transform", "translate(0,30)")
-            .call(axis);
-            */
-
-    /*
-        var yaxis_sel = svgContainer.select("yaxis")
-            .data(means)
-            .enter()
-            .append("ypoint");
-            */
-
+       
         var x = d3.scale.linear()
             .domain([starttime, endtime])
             .range([0, width]);
 
-        var max = Math.max.apply(Math, means),
-            min = Math.min.apply(Math, means);
+        var max = Math.max.apply(Math, maxes),
+            min = Math.min.apply(Math, mins);
 
         var y = d3.scale.linear()
             .domain([min,max])
             .range([height,0]);
 
-        var dx = d3.scale.linear()
-            .range([0, width-100]);
-
-        var xaxis = d3.svg.axis()
+        var xAxis = d3.svg.axis()
             .scale(x)
             .orient("bottom");
 
-        var yaxis = d3.svg.axis()
+        var yAxis = d3.svg.axis()
             .scale(y)
             .orient("left");
 
         var line = d3.svg.line()
-            //.x(function(d) { return (d.mt).toFixed(2); })
-            //.y(function(d) { return (d.mean).toFixed(2); })
             .x(function(d) { return x(d.mt); })
             .y(function(d) { return y(d.mean); })
             .interpolate("linear");
 
-        var xaxis_graph = svgContainer.append("g")
+        var xAxis_graph = svgContainer.append("g")
             .attr("class", "x_axis")
-            .attr("transform", "translate(" + 0 + "," + (height) + ")")
-            .call(xaxis);
+            .attr("transform", "translate(" + 0 + "," + height + ")")
+            .call(xAxis);
 
-        var yaxis_graph = svgContainer.append("g")
+        var yAxis_graph = svgContainer.append("g")
             .attr("class", "y_axis")
-            .attr("transform", "translate(" + 0 + "," + 0 + ")")
-            .call(yaxis);
+            .call(yAxis);
 
-        var linegraph = svgContainer.append("path")
+        var lineGraph = svgContainer.append("path")
             .attr("class", "line")
-            //.attr("clip-path", "url(#clip)")
-            .attr("transform", "translate(" + 0 + "," + 0 + ")")
+            .attr("clip-path", "url(#clip)")
             .attr("d", line(sample));
+
+        /* Begin zooming */
+
+        var zoom = d3.behavior.zoom()
+            .on("zoom", draw);
+
+        svgContainer.append("clipPath")
+            .attr("id", "clip")
+        .append("rect")
+            .attr("x", 0)
+            .attr("y", 0)
+            .attr("width", width)
+            .attr("height", height);
+
+        svgContainer.append("rect")
+            .attr("class", "pane")
+            .attr("width", width)
+            .attr("height", height)
+            .call(zoom);
+
+        zoom.x(x);
+
+        function draw() {
+            svgContainer.select("g.x.axis").call(xAxis);
+            svgContainer.select("g.y.axis").call(yAxis);
+            svgContainer.select("path.line").attr("d",line(sample));
+        }
+
+        draw();
     });
 });
